a5217914af3cfc649cc2dbdb69260e7c060f292c
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-07-16 21:07:07 +0530

    Add files via upload

---
+++ Q28.py
@@ -0,0 +1,17 @@
+str1 = "I do you do and we all will do"
+str1 = str1.replace("do","done")
+print(str1)
+
+# here we use replace function and chane do to done
+
+
+string = input("enter any setence=")
+i = -1
+reverse_str = ""
+while i >= (-len(string)):
+    reverse_str += string[i]
+    i -= 1
+print(reverse_str)
+
+# reverse of string
+
